b4f6d225e42d6f6581f8f9f0b2db8cad7043db3c
qiyanhe_35923 <zhou42538@outlook.com>
2023-11-05 01:19:36 +0800

    11.5

---
+++ 杂七杂八/prac.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//第一题 门牌制作 
+
+//int calu(int num){
+//	int k,res=0;
+//	while(num){
+//		k=num%10;
+//		if(k==2)	res++;
+//		num/=10;
+//	}
+//	return res;
+//}
+//
+//int main(){
+//	int res=0;
+//	for(int i=1;i<=2020;i++){
+//		res+=calu(i);
+//	}
+//	cout<<res<<endl;
+//} 
+
+//第二题 既约分数
+
+//int gcd(int x, int y){
+//	if(!y)	return x;
+//	else	gcd(y, x%y);
+//}
+//
+//int main(){
+//	int res=0;
+//	for(int i=1;i<=2020;i++){
+//		for(int j=1;j<=2020;j++){
+//			if(gcd(i,j)==1)	res++;
+//		}
+//	}
+//	cout<<res<<endl;
+//	return 0;
+//} 
+
+
+//第三题 蛇形填数
+
+//int main(){
+//	int r,c;//行 列
+//	cin>>r>>c;
+//	r=2*r-1;
+//	int ans = 0,sum =  0;
+//	for(int i = 1; i<=r ;i++)
+//		sum+=i; 
+//	ans = (sum+sum-r+1)/2;
+// 	cout<<ans<<endl;
+//	return 0;
+//} 
+
+//第六题 成绩统计
+
+int main(){
+	return 0;
+} 
