5336285668518b4bbe3556291351044b04ec4800
qiyanhe_35923 <zhou42538@outlook.com>
2023-11-05 12:44:33 +0800

    11.5

--- 杂七杂八/prac.cpp
+++ 杂七杂八/prac.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//第一题 门牌制作 
+//门牌制作 
 
 //int calu(int num){
 //	int k,res=0;
@@ -21,7 +21,7 @@ using namespace std;
 //	cout<<res<<endl;
 //} 
 
-//第二题 既约分数
+//既约分数
 
 //int gcd(int x, int y){
 //	if(!y)	return x;
@@ -54,8 +54,97 @@ using namespace std;
 //	return 0;
 //} 
 
-//第六题 成绩统计
+//成绩统计
+
+//int n, maxx=0, minn=INT_MAX;
+//double sum; 
+//
+//int main(){
+//	cin>>n;
+//	for(int i=0;i<n;i++){
+//		int x;cin>>x;
+//		if(x>maxx)	maxx=x;
+//		if(x<minn)	minn=x;
+//		sum+=x;
+//	}
+//	cout<<maxx<<endl;
+//	cout<<minn<<endl;
+//	double avg=sum/n;
+//	printf("%.2lf",avg); 
+//	return 0;
+//} 
+
+
+//字符串排序 
+
+//获取长度为 m 的字符串的最大逆序对数
+int f[1010],n;
+int get_max(){
+	for(int i=2;i<=26;i++)	f[i]=f[i-1]+i-1;
+	int sum=26,vis[30];
+	//记录当前字符串已经给各个字符串一个了 
+	for(int i=0;i<26;i++)	vis[i]=1;
+	for(int i=27;f[i-1]<n;i++,sum++){
+		int ch=(i%26-1+26)%26;
+		//总字符数目-相同的字符数目 
+		f[i]=f[i-1]+sum-vis[ch];vis[ch]++;
+	}
+} 
+
+//cnt代表已经确定的构造符序列 
+int cnt[30],vis[30];
+int get_add(int ch){
+	int add=0;
+	for(int i=0;i<ch;i++)	add+=vis[i];
+	for(int i=ch+1;i<26;i++)	add+=cnt[i]+vis[i];
+	return add;
+}
+
+bool check(int id, int m, int ch, int sum){
+	for(int i=id+1;i<=m;i++){
+		int maxadd=0,chl=0;
+		for(int j=0;j<26;j++){
+			int add=get_add(j);
+			if(maxadd<add){
+				maxadd=add;
+				chl=j;
+			}
+		} 
+		vis[chl]++;
+		sum+=maxadd;
+	}
+	memset(vis,0,sizeof vis);
+	if(sum>=n)	return true;
+	else	false;
+}
+
+//m为字符串的长度 
+void solve(int m){
+	int sum=0;string ans;
+	for(int i=1;i<=m;i++){
+		for(int j=0;j<26;j++){
+			int initadd=get_add(j);
+			cnt[j]++;
+			sum+=initadd;
+			if(check(i,m,j,sum)){
+				ans+=('a'+j);
+				break;
+			}
+			cnt[j]--;
+			sum-=initadd;
+		}
+	}
+	cout<<ans<<endl;
+}
 
 int main(){
+	cin>>n;
+	get_max();
+	for(int i=2;i<=n;i++){
+		if(f[i]>=n){
+			solve(i);
+			break;
+		}
+	}
 	return 0;
-} 
+}
