ac32912b5afa0f8f2391db32d0ad1239bcbe0e18
qiyanhe_35923 <zhou42538@outlook.com>
2023-11-05 23:18:24 +0800

    11.5

--- 杂七杂八/prac.cpp
+++ 杂七杂八/prac.cpp
@@ -75,76 +75,96 @@ using namespace std;
 //} 
 
 
+
 //字符串排序 
-
 //获取长度为 m 的字符串的最大逆序对数
-int f[1010],n;
-int get_max(){
-	for(int i=2;i<=26;i++)	f[i]=f[i-1]+i-1;
-	int sum=26,vis[30];
-	//记录当前字符串已经给各个字符串一个了 
-	for(int i=0;i<26;i++)	vis[i]=1;
-	for(int i=27;f[i-1]<n;i++,sum++){
-		int ch=(i%26-1+26)%26;
-		//总字符数目-相同的字符数目 
-		f[i]=f[i-1]+sum-vis[ch];vis[ch]++;
-	}
-} 
+//int f[1010],n;
+//int get_max(){
+//	for(int i=2;i<=26;i++)	f[i]=f[i-1]+i-1;
+//	int sum=26,vis[30];
+//	//记录当前字符串已经给各个字符串一个了 
+//	for(int i=0;i<26;i++)	vis[i]=1;
+//	for(int i=27;f[i-1]<n;i++,sum++){
+//		int ch=(i%26-1+26)%26;
+//		//总字符数目-相同的字符数目 
+//		f[i]=f[i-1]+sum-vis[ch];vis[ch]++;
+//	}
+//} 
+//
+////cnt代表已经确定的构造符序列 
+//int cnt[30],vis[30];
+//int get_add(int ch){
+//	int add=0;
+//	for(int i=0;i<ch;i++)	add+=vis[i];
+//	for(int i=ch+1;i<26;i++)	add+=cnt[i]+vis[i];
+//	return add;
+//}
+//
+//bool check(int id, int m, int ch, int sum){
+//	for(int i=id+1;i<=m;i++){
+//		int maxadd=0,chl=0;
+//		for(int j=0;j<26;j++){
+//			int add=get_add(j);
+//			if(maxadd<add){
+//				maxadd=add;
+//				chl=j;
+//			}
+//		} 
+//		vis[chl]++;
+//		sum+=maxadd;
+//	}
+//	memset(vis,0,sizeof vis);
+//	if(sum>=n)	return true;
+//	else	false;
+//}
+//
+////m为字符串的长度 
+//void solve(int m){
+//	int sum=0;string ans;
+//	for(int i=1;i<=m;i++){
+//		for(int j=0;j<26;j++){
+//			int initadd=get_add(j);
+//			cnt[j]++;
+//			sum+=initadd;
+//			if(check(i,m,j,sum)){
+//				ans+=('a'+j);
+//				break;
+//			}
+//			cnt[j]--;
+//			sum-=initadd;
+//		}
+//	}
+//	cout<<ans<<endl;
+//}
+//
+//int main(){
+//	cin>>n;
+//	get_max();
+//	for(int i=2;i<=n;i++){
+//		if(f[i]>=n){
+//			solve(i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
 
-//cnt代表已经确定的构造符序列 
-int cnt[30],vis[30];
-int get_add(int ch){
-	int add=0;
-	for(int i=0;i<ch;i++)	add+=vis[i];
-	for(int i=ch+1;i<26;i++)	add+=cnt[i]+vis[i];
-	return add;
-}
 
-bool check(int id, int m, int ch, int sum){
-	for(int i=id+1;i<=m;i++){
-		int maxadd=0,chl=0;
-		for(int j=0;j<26;j++){
-			int add=get_add(j);
-			if(maxadd<add){
-				maxadd=add;
-				chl=j;
-			}
-		} 
-		vis[chl]++;
-		sum+=maxadd;
-	}
-	memset(vis,0,sizeof vis);
-	if(sum>=n)	return true;
-	else	false;
-}
 
-//m为字符串的长度 
-void solve(int m){
-	int sum=0;string ans;
-	for(int i=1;i<=m;i++){
-		for(int j=0;j<26;j++){
-			int initadd=get_add(j);
-			cnt[j]++;
-			sum+=initadd;
-			if(check(i,m,j,sum)){
-				ans+=('a'+j);
-				break;
-			}
-			cnt[j]--;
-			sum-=initadd;
-		}
-	}
-	cout<<ans<<endl;
-}
-
+//贪心
 int main(){
-	cin>>n;
-	get_max();
-	for(int i=2;i<=n;i++){
-		if(f[i]>=n){
-			solve(i);
-			break;
-		}
+	int n;
+	vector<int> start,ends; 
+	for(int i=0;i<n;i++){
+		cin>>start[i]>>ends[i];
+	} 
+	sort(start.begin(), start.end());
+	sort(ends.begin(), ends.end());
+	int res=0,j=0;
+	for(int i=0;i<n;i++){
+		if(start[i]>=ends[j])	j++;
+		else	res++;
 	}
+	cout<<res<<endl;
 	return 0;
-}
+} 
