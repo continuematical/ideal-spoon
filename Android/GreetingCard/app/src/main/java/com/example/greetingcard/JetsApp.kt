1332a4118ae365ac34fd10fca5c0c073ab4f59ff
qiyanhe_35923 <zhou42538@outlook.com>
2023-11-18 23:28:40 +0800

    11.18

---
+++ Android/GreetingCard/app/src/main/java/com/example/greetingcard/JetsApp.kt
@@ -0,0 +1,8 @@
+package com.example.greetingcard
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JetsApp(){
+    
+}
--- Android/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ Android/GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -3,30 +3,32 @@ package com.example.greetingcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GreetingCardTheme {
-                Surface (color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()){
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.size(100.dp)
+                ) {
                     DefaultPreview()
                 }
             }
@@ -37,8 +39,14 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Hello", modifier = Modifier.padding(bottom = 8.dp))
-        TextField(value = "world", onValueChange = {})
+    var countNumber by remember {
+        mutableStateOf(0)
+    }
+
+    Column {
+        Text(text = "当前计数器：$countNumber")
+        Button(onClick = { countNumber++ }) {
+            Text(text = "增加计数")
+        }
     }
 }
